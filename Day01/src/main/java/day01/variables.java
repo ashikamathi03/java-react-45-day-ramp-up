fa33a0e20319d30df314fb3355d8cc09e13c6178
ashikamathi03 <ashikamathi03@gmail.com>
2026-08-27 21:57:21 +0530

    Add variables.java

---
+++ Day01/src/main/java/day01/variables.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class variables {
+    public static void main(String[] args){
+        String name="Ashika";
+        int age=23;
+        boolean isGraduate=false;
+        double sal=40000.80;
+        char grade='A';
+
+        System.out.println("Name: " +name);
+        System.out.println("age: " +age);
+        System.out.println("isgraduate: " +name);
+        System.out.println("Age: " +age);
+        System.out.println("grade: " +grade);
+    }
+}
